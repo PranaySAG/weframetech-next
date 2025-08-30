b96226d47d555ba1c079a7d29a6b5155dc7859d4
PranaySAG <pranayshinde929@gmail.com>
2025-08-30 14:31:55 +0530

    fix height and width in dashboardcard and documenttable

--- app/component/DocumentTable.tsx
+++ app/component/DocumentTable.tsx
@@ -71,7 +71,7 @@ export default function DocumentTable() {
             placeholder="     Search here..."
             className="py-[10px] px-[14px] h-[44px] rounded-lg border-[#999999] w-[320px] text-sm focus:outline-none focus:ring-2 focus:ring-sky-400"
           />
-          <svg className="absolute left-1  w-5 h-5 text-gray-400" viewBox="0 0 24 24" fill="none" stroke="currentColor">
+          <svg className="absolute left-1 top-3  w-5 h-5 text-gray-400" viewBox="0 0 24 24" fill="none" stroke="currentColor">
             <path strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" d="M21 21l-4.35-4.35" />
             <circle cx="11" cy="11" r="6" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
           </svg>
